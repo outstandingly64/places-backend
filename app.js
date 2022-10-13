e0129c829846b10e4b4c63e7494fe8c49fde49af
Ivan Tello <ivantello9922@gmail.com>
2022-10-13 17:35:01 -0400

    connection to db established

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const placesRoutes = require("./routes/places-routes");
+const userRoutes = require("./routes/user-routes");
+const HttpError = require("./models/http-error");
 
-const placesRoutes = require('./routes/places-routes');
-const userRoutes = require('./routes/user-routes');
-const HttpError = require('./models/http-error');
+const PLACES_DB_URL = require("./util/config");
 
 // it is good practice NOT to do the routing
 // in this main app.js file...
@@ -14,23 +16,30 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places' ,placesRoutes); // =>/api/places/...
+app.use("/api/places", placesRoutes); // =>/api/places/...
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 //error handling for unsupported routes
-app.use((req, res, next)=>{
-    const error = new HttpError('Could not find this route', 404);
-    throw error;
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
 });
 
-app.use((error, req, res, next)=>{
-    if(res.headerSent){
-        return next(error);
-    }
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
 
-    res.status(error.code || 500)
-    res.json({message: error.message || "An Unknown Error Has Occurred"});
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An Unknown Error Has Occurred" });
 });
 
-app.listen(5000)
+mongoose
+  .connect(PLACES_DB_URL)
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
