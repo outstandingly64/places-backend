8f2e1a8f39ff22be2586d28452c1180c5e87343d
Ivan Tello <ivantello9922@gmail.com>
2022-10-22 15:35:27 -0400

    CORS error fix

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/places", placesRoutes); // =>/api/places/...
 
 app.use("/api/users", userRoutes);
