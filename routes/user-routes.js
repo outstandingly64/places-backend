31497c4ef815ed8f73237215f9a9bc210f2fdfd7
Ivan Tello <ivantello9922@gmail.com>
2022-10-28 18:12:07 -0400

    configuring multer middleware to filter files

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get("/", usersControllers.getUsers);
 
-router.post("/signup", [
+router.post("/signup", fileUpload.single('image'), [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
     check('password').isLength({ min: 6})
