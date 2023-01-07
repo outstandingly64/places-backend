300ef2e42fbecb2a02488117b06bf89a7238c4a3
Ivan T <112672027+outstandingly64@users.noreply.github.com>
2023-01-06 19:17:43 -0500

    (refactor) mongodb url template string

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const placesRoutes = require("./routes/places-routes");
 const userRoutes = require("./routes/user-routes");
 const HttpError = require("./models/http-error");
 
-const PLACES_DB_URL = require("./util/databaseUrl");
 const { ifError } = require('assert');
 const { fields } = require('./middleware/file-upload');
 
@@ -56,7 +55,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(PLACES_DB_URL)
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ym50pep.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(process.env.PORT || 5000);
   })
--- util/databaseUrl.js
+++
@@ -1,4 +0,0 @@
-const PLACES_DB_URL =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ym50pep.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-module.exports = PLACES_DB_URL;
